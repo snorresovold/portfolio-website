8dec6fe14e313d470ac31506b8fd3677564dfcbe
snorresovold <snorresovold@gmail.com>
2021-12-08 18:47:18 +0100

    started to work on the navbar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       Hello
+      <Navbar />
     </div>
   );
 }
