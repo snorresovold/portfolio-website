180b4e9e12cfdf29e5f5bed57a28391689fc9a57
snorresovold <snorresovold@gmail.com>
2021-12-08 19:57:14 +0100

    deleted navbar to make way for a threejs project

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
     </div>
   );
 }
