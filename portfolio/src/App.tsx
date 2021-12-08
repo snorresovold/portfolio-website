56dd83843a28bc1a08f34612854f2f39731cc4d0
snorresovold <snorresovold@gmail.com>
2021-12-08 20:38:13 +0100

    updates

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Homepage from './pages/Homepage';
 
 
 function App() {
   return (
     <div className="App">
+      <Homepage />
     </div>
   );
 }
---
+++ portfolio/src/pages/Homepage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Homepage.css"
+
+function Homepage() {
+    return (
+        <div>
+            <div className="container">
+                <h1 className="title">WELCOME</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage
